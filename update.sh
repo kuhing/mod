a4cdf98d6b37443d5e9cc2a320c3955988871430
kuhing <134251508+kuhing@users.noreply.github.com>
2023-07-05 22:08:32 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -15,93 +15,92 @@ WB='\e[37;1m'
 clear
 echo -e "${GB}[ INFO ]${NC} ${YB}Downloading File Menu${NC}"
 cd /usr/bin
-sleep 2
+sleep 1
+echo-e ""
 wget -q -O menu "https://raw.githubusercontent.com/kuhing/menu.sh/menu.sh/menu.sh"
-clear
+echo -e "${GB}[ INFO ]${NC} ${YB}Sambil Nunggu.....!!!!${NC}"
 wget -q -O menu-update "https://raw.githubusercontent.com/jambanbkn/menu-update.sh/menu-update.sh/menu-update.sh"
-clear
+echo -e ""
 wget -q -O m-bot "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/m-bot.sh"
-clear
+echo -e "${GB}[ INFO ]${NC} ${YB}Minum Kopi + Rokok Dulu Brooooo${NC}"
 wget -q -O m-ip "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/m-ip.sh"
-clear
+echo -e ""
 wget -q -O menu-vmess "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-vmess.sh"
-clear
+
 wget -q -O menu-theme "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-theme.sh"
-clear
+
 wget -q -O menu-backup "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-backup.sh"
-clear
+
 wget -q -O menu-uuid "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-uuid.sh"
-clear
+echo -e "${GB}[ INFO ]${NC} ${YB}Heheeeeeeee.....${NC}"
 wget -q -O menu-vmess "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-vmess.sh"
-clear
+
 wget -q -O menu-vless "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-vless.sh"
-clear
+
 wget -q -O menu-ssws "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-ssws.sh"
-clear
+
 wget -q -O running "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/running.sh"
-clear
+
 wget -q -O clearcache "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/clearcache.sh"
-clear
+
 wget -q -O menu-trgo "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-trgo.sh"
-clear
+
 wget -q -O menu-trojan "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-trojan.sh"
-clear
+
 wget -q -O menu-ssh "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-ssh.sh"
-clear
+
 wget -q -O menu-set "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-set.sh"
-clear
+
 wget -q -O menu-domain "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-domain.sh"
-clear
+
 wget -q -O add-host "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/ssh/add-host.sh"
-clear
+
 wget -q -O port-change "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/port/port-change.sh"
-clear
+
+wget -q -O port-ovpn "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/port/port-ovpn.sh"
+
 wget -q -O certv2ray "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/xray/certv2ray.sh"
-clear
+
 wget -q -O menu-webmin "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-webmin.sh"
-clear
+
 wget -q -O speedtest "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/ssh/speedtest_cli.py"
-clear
+
 wget -q -O about "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/about.sh"
-clear
+
 wget -q -O auto-reboot "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/auto-reboot.sh"
-clear
+
 wget -q -O restart "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/restart.sh"
-clear
+
 wget -q -O bw "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/bw.sh"
-clear
+
 wget -q -O port-ssl "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/port/port-ssl.sh"
-clear
+
 wget -q -O xp "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/ssh/xp.sh"
-clear
-wget -q -O acs-set "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/acs-set.sh"
-clear
-wget -q -O sshws "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/ssh/sshws.sh"
-clear
+
 wget -q -O status "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/status.sh"
-clear
+
 wget -q -O menu-bckp "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-bckp.sh"
-clear
+
 wget -q -O backup "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/backup.sh"
-clear
+
 wget -q -O restore "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/restore.sh"
-clear
+
 wget -q -O jam "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/jam.sh"
-clear
+
 wget -q -O addtrgo "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/xray/addtrgo.sh"
-clear
+
 wget -q -O trialtrojango "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/xray/trialtrojango.sh"
-clear
+
 wget -q -O deltrgo "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/xray/deltrgo.sh"
-clear
+
 wget -q -O renewtrgo "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/xray/renewtrgo.sh"
-clear
+
 wget -q -O cektrgo "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/xray/cektrgo.sh"
-clear
+
 wget -q -O user-lock "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/ssh/user-lock.sh"
-clear
+
 wget -q -O user-unlock "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/ssh/user-unlock.sh"
-clear
+
 
 chmod +x menu
 chmod +x menu-update
@@ -130,8 +129,6 @@ chmod +x restart
 chmod +x bw
 chmod +x port-ssl
 chmod +x port-ovpn
-chmod +x acs-set
-chmod +x sshws
 chmod +x status
 chmod +x menu-bckp
 chmod +x backup
@@ -146,6 +143,6 @@ chmod +x cektrgo
 chmod +x user-lock
 chmod +x user-unlock
 echo -e "${GB}[ INFO ]${NC} ${YB}Downloading File Menu Successfully${NC}"
-sleep 2
+sleep 5
 cd
 clear
